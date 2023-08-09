e1939a618ac5a53e70db6554e189d07f15b38c3b
Henri Meriläinen <henri.merilainen1@gmail.com>
2023-08-09 18:39:05 +0300

    fixed tests and commit

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -1,24 +1,29 @@
 import codecs
 
 def encode(s):
-    if not isinstance(s,str):
+    if not isinstance(s, str):
         raise TypeError
     origlen = len(s)
     crypted = ""
-    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+    digitmapping = dict(zip('1234567890!"#€%&/()=', '!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
+    s = s.ljust(1000,"x")
     for c in s:
+        if c in ["ä", "ö", "å"]:
+            raise ValueError
         if c.isalpha():
             if c.islower():
-                c=c.upper()
+                c = c.upper()
             # Rot13 the character for maximum security
-            crypted+=codecs.encode(c,'rot13')
+            crypted += codecs.encode(c, 'rot13')
         elif c in digitmapping:
-          crypted+=digitmapping[c]
+            crypted += digitmapping[c]
+        else:
+            raise ValueError
 
-    return crypted
+    return crypted[:origlen]
 
 def decode(s):
-    return s
+    return encode(s).lower()
 
--- lab2/test_mycrypt.py
+++ lab2/test_mycrypt.py
@@ -42,10 +42,16 @@ def test_invalid_char(invalid_input):
         mycrypt.encode(invalid_input)
 
 
-@pytest.mark.parametrize("invalid_input", [])
+@pytest.mark.parametrize("invalid_input", [1, 2.5])
 def test_invalid_types(invalid_input):
     '''Invalid parameter types should raise TypeError'''
     with pytest.raises(TypeError):
+        mycrypt.encode(invalid_input)
+        
+@pytest.mark.parametrize("invalid_input", ["2"*4000, "3"*2000])
+def test_length(invalid_input):
+    '''Invalid input parameter length should raise ValueError'''
+    with pytest.raises(ValueError):
         mycrypt.encode(invalid_input)
 
 
